f84ea05751ecd4c0fa79f13c5d7105b4788083d4
blunlove <314884680@qq.com>
2017-06-23 21:48:23 +0800

    更新上一条

--- 任务二/Game.js
+++ 任务二/Game.js
@@ -4,9 +4,12 @@ function reflash () {
 	$(".Snake").css({'teft':Snake.lastPositionX[1]*Snake.size,'top':Snake.lastPositionY[1]*Snake.size})
 }
 var start =setInterval(reflash,frameTime);*/
+var worldSizeX=800;
+var worldSizeY=600;
+$("#World").css({'width':worldSizeX,'height': worldSizeY});
 foodRest();
 rest();
-$("#theScore").text(Snake.length);
+$("#theScore").css({'left':(worldSizeX/2-25),'top':(worldSizeY+50)}).text(Snake.length);
 var speed=100;
 var running =setInterval(Running,speed);
 function GameOver () {
