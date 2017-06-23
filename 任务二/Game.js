17a53bead5b0029295fa96978714955ff7218b18
blunlove <314884680@qq.com>
2017-06-23 19:59:47 +0800

    不能后退功能

--- 任务二/Game.js
+++ 任务二/Game.js
@@ -1,18 +1,19 @@
-var lastTime=new Date();
-var frameRate=10;
-var frameTime=1000/frameRate;;
-/*function update () {
-		Running();
-}*/
-var start =setInterval(Running,frameTime);
+/*var frameRate=60;
+var frameTime=1000/frameRate;
+function reflash () {
+	$(".Snake").css({'teft':Snake.lastPositionX[1]*Snake.size,'top':Snake.lastPositionY[1]*Snake.size})
+}
+var start =setInterval(reflash,frameTime);*/
+var speed=100;
+var running =setInterval(Running,speed);
+function rest() {
+    Snake.direction=0;
+    Snake.lastDirection=0;
+    Snake.lastPositionX[0]=parseInt((Math.random()*Snake.maxX));
+    Snake.lastPositionY[0]=parseInt((Math.random()*Snake.maxY));
+    Snake.lastPositionX[1]=Snake.lastPositionX[0];
+    Snake.lastPositionY[1]=Snake.lastPositionY[0];
+    $(".Snake").css({'left':(Snake.lastPositionX[0]*Snake.size),'top':(Snake.lastPositionY[0]*Snake.size)});
 
-
-/*function start(){
-	var thisTime = new Date();
-	if (thisTime.getTime() - lastTime.getTime()>=frameTime) {
-		lastTime=thisTime;
-		update();
-	}
-	start();
-}*/
-//start();
+}
+rest();
--- 任务二/Snake.js
+++ 任务二/Snake.js
@@ -6,55 +6,70 @@ Snake.maxX=worldSizeX/Snake.size-1;
 Snake.maxY=worldSizeY/Snake.size-1;
 Snake.lastPositionX=new Array();
 Snake.lastPositionY=new Array();
-function rest() {
-    Snake.direction=0;
-    Snake.lastPositionX[0]=parseInt((Math.random()*Snake.maxX));
-    Snake.lastPositionY[0]=parseInt((Math.random()*Snake.maxY));
-    Snake.lastPositionX[1]=Snake.lastPositionX[0];
-    Snake.lastPositionY[1]=Snake.lastPositionY[0];
-    $(".Snake").css({'left':(Snake.lastPositionX[0]*Snake.size),'top':(Snake.lastPositionY[0]*Snake.size)});
-
-}
-rest();
 $(document).keydown(function(event){
     if (event.keyCode==37) {
+        if (Snake.direction==3) {return;}
         Snake.direction = 1;//left
     }
     if (event.keyCode==38) {
+        if (Snake.direction==4) {return;}
         Snake.direction = 2;//top
     }
     if (event.keyCode==39) {
+        if (Snake.direction==1) {return;}
         Snake.direction = 3;//right
     }
     if (event.keyCode==40) {
+        if (Snake.direction==2) {return;}
         Snake.direction = 4;//down
     }
 });
 function Running () {
-    if(Snake.lastPositionX[1]==0 || Snake.lastPositionX[1]==Snake.maxX || Snake.lastPositionY[1]==0||Snake.lastPositionY[1]==Snake.maxY){
+    /*if(Snake.lastPositionX[1]==0 || Snake.lastPositionX[1]==Snake.maxX || Snake.lastPositionY[1]==0||Snake.lastPositionY[1]==Snake.maxY){
         //$(".Snake").css({'left':(Snake.lastPositionX[1])*Snake.size,'top':(Snake.lastPositionY[1])*Snake.size});
         alert("Game Over...");
         rest();
         return;
-    }
+    }*/
     if (Snake.direction==1) {
+        Snake.lastDirection=Snake.direction;
         Snake.lastPositionX[1]=Snake.lastPositionX[0];
         Snake.lastPositionX[0]=Snake.lastPositionX[0]-1;
         $(".Snake").css({'left':(Snake.lastPositionX[0])*Snake.size});
+        if(Snake.lastPositionX[1]==0){
+            alert("Game Over...");
+            rest();
+            return;
+        }
     }
     if (Snake.direction==3) {
         Snake.lastPositionX[1]=Snake.lastPositionX[0];
         Snake.lastPositionX[0]=Snake.lastPositionX[0]+1;
         $(".Snake").css({'left':(Snake.lastPositionX[0])*Snake.size});
+        if(Snake.lastPositionX[1]==Snake.maxX){
+            alert("Game Over...");
+            rest();
+            return;
+        }
     }
     if (Snake.direction==2) {
         Snake.lastPositionY[1]=Snake.lastPositionY[0];
         Snake.lastPositionY[0]=Snake.lastPositionY[0]-1;
         $(".Snake").css({'top':(Snake.lastPositionY[0])*Snake.size});
+        if(Snake.lastPositionY[1]==0){
+            alert("Game Over...");
+            rest();
+            return;
+        }
     }
     if (Snake.direction==4) {
         Snake.lastPositionY[1]=Snake.lastPositionY[0];
         Snake.lastPositionY[0]=Snake.lastPositionY[0]+1;
         $(".Snake").css({'top':(Snake.lastPositionY[0])*Snake.size});
+        if(Snake.lastPositionY[1]==Snake.maxY){
+            alert("Game Over...");
+            rest();
+            return;
+        }
     }
 }
