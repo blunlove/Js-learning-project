e2dd2ae340d9539d396e9228deb8c25dfc3fd8f2
blunlove <314884680@qq.com>
2017-06-23 16:44:03 +0800

    修改

--- 任务二/Game.js
+++ 任务二/Game.js
@@ -3,18 +3,14 @@ var lastTime=new Date();
 var frameRate=10;
 var frameTime=1000/frameRate;;
 function update () {
-    if (Snake.direction == 1) {
-      Snake.lastPositionX=Snake.lastPosition-1;
-        $("Snake").css({'left':Snake.lastPositionX*Snake.size,'top':Snake.lastpositonY});
-    }
-    //if(!0<Snake.lastPositionX<(Snake.maxX-1) && )
-        }
-
-while (true) {
+		Running();
+}
+function start(){
 	var thisTime = new Date();
 	if (thisTime.getTime() - lastTime.getTime()>=frameTime) {
 		lastTime=thisTime;
 		update();
 	}
+	start();
 }
-
+//start();
--- 任务二/Snake.js
+++ 任务二/Snake.js
@@ -1,14 +1,19 @@
+var isRunning=false;
 Snake=new Object();
-//Snake.Speed=500;
+Snake.Speed=100;
 Snake.size=20;
-Snake.maxX=worldSizeX/Snake.size;
-Snake.maxY=worldSizeY/Snake.size;
-Snake.direction;
-Snake.beginPositionX=parseInt((Math.random()*Snake.maxX-1));
-Snake.beginPositionY=parseInt((Math.random()*Snake.maxY-1));
-Snake.lastPositionX=Snake.beginPositionX;
-Snake.lastPositionY=Snake.beginPositionY;
-$("#Snake").css({'left':Snake.beginPositionX*Snake.size,'top':Snake.beginPositionY*Snake.size})
+$(".Snake").css({'width':Snake.size,'height':Snake.size});
+Snake.maxX=worldSizeX/Snake.size-1;
+Snake.maxY=worldSizeY/Snake.size-1;
+Snake.direction=0;
+Snake.beginPositionX=parseInt((Math.random()*Snake.maxX));
+Snake.beginPositionY=parseInt((Math.random()*Snake.maxY));
+Snake.lastPositionX=new Array();
+Snake.lastPositionY=new Array();
+Snake.lastPositionX[0]=Snake.beginPositionX;
+Snake.lastPositionY[0]=Snake.beginPositionY;
+
+$(".Snake").css({'left':(Snake.beginPositionX*Snake.size),'top':(Snake.beginPositionY*Snake.size)});
 $(document).keydown(function(event){
     if (event.keyCode==37) {
         Snake.direction = 1;//left
@@ -23,67 +28,17 @@ $(document).keydown(function(event){
         Snake.direction = 4;//down
     }
 });
-        /*function Running(x,y){
-            if(direction == 37 && x>=0){
-                newX=x-1;
-                newY=y;
-                lastPositionX=x;
-                //$(".Snake").delay(Speed).animate({'left': (newX*20)},0,function(){
-                //    Running(newX,newY);
-                //});
-            }
-            else if(direction == 38 && y>=0){
-                newX=x;
-                newY=y-1;
-                lastPositionY=y;
-                //$(".Snake").delay(Speed).animate({'top': (newY*20)},0,function(){
-                //    Running(newX,newY);
-                //});
-            }
-            else if(direction == 39 && x<=39){
-                newX=x+1;
-                newY=y;
-                lastPositionX=x;
-                //$(".Snake").delay(Speed).animate({'left': (newX*20)},0,function(){
-                //    Running(newX,newY);
-                //});
-            }
-            else if(direction == 40 && y<=29){
-                newX=x;
-                newY=y+1;
-                lastPositionY=y;
-                //$(".Snake").delay(Speed).animate({'top': (newY*20)},0,function(){
-                //    Running(newX,newY);
-                //});
-            }
-                $(".Snake").delay(Speed).animate({'left':(newX*20),'top': (newY*20)},0,function(){
-                    if (newX==-1||newX==40||newY==-1||newY==30) 
-                    {
-                        $(".Snake").css({"left":lastPositionX*20,"top":lastPositionY*20});
-                        alert("Game Over...");
-                        return;
-                    }
-                    Running(newX,newY);
-                });
-        }*/
-        //$(document).keydown(function(event){
-            //isRunning=false;
-            //direction=event.keyCode;
-            //if(!isRunning){
-              //  isRunning=true;
-                //Running(beginPositionX,beginPositionY);
-            //}
-            //left
-            //if(event.keyCode == 38 && beginPositionY>0){
-               // beginPositionY=beginPositionY;
-                //$(".Snake").css({'top':(beginPositionY)});
-            //}//up
-            //if(event.keyCode == 39 && beginPositionX<780){
-                //beginPositionX=beginPositionX;
-                //$(".Snake").css({'left':(beginPositionX)});
-            //}//right
-            //if(event.keyCode == 40 && beginPositionY<580){
-                //beginPositionY=beginPositionY;
-                //$(".Snake").css({'top':(beginPositionY)});
-            //}//down
-       // });
+function Running () {
+    if(!isRunning){
+        isRunning=true;
+        if (Snake.direction==1 && 0<Snake.lastPositionX[0]<Snake.maxX) {
+            Snake.lastPositionX[0]=Snake.lastPositionX[0]-1;
+            $(".Snake").delay(Snake.Speed).animate({'left':Snake.lastPositionX[0]*Snake.size},0);
+        }
+        if (Snake.direction==3 && 0<Snake.lastPositionX[0]<Snake.maxX) {
+            Snake.lastPositionX[0]=Snake.lastPositionX[0]+1;
+            $(".Snake").delay(Snake.Speed).animate({'left':Snake.lastPositionX[0]*Snake.size},0);
+        }
+        isRunning=false;
+    }
+}
