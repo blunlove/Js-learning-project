826333c204af2237aa398bb173cc89f2eb781413
blunlove <314884680@qq.com>
2017-06-25 21:22:51 +0800

    game方法封装

--- 任务二/Game.js
+++ 任务二/Game.js
@@ -1,67 +1,69 @@
-var worldSizeX =800;
-var worldSizeY=600;
-var snakeSize=20;
-var foodSize=20;
-var beginFramr=2;
-var frame = beginFramr;
-var speed = 1000/frame;
-var maxX=worldSizeX/snakeSize-1;
-var maxY=worldSizeY/snakeSize-1;
-var theWorld=$("#World");
-var theScore=$("#theScore");
-var tempKey;
-theWorld.css({'width':worldSizeX,'height': worldSizeY});
-theScore.css({'left':(worldSizeX/2-25),'top':(worldSizeY+50)}).text("0");
-var snake=new Snake(theWorld,'red','rgb(150,0,0)',snakeSize,snakeSize,maxX,maxY);
-snake.rest();
-var food=new Food(theWorld,'green',foodSize,foodSize,maxX,maxY);
-food.rest();
-if (food.x==snake.x&&food.y==snake.y)food.rest();
-function start(){
-	if (tempKey==37) {
-        snake.turnLeft();//left
-    }
-    if (tempKey==38) {
-        snake.turnUp();//top
-    }
-    if (tempKey==39) {
-        snake.turnRight();;//right
-    }
-    if (tempKey==40) {
-        snake.turnDown();//down
-    }
-    snake.update();
-    if(snake.eat(food.x,food.y)){
-        theScore.text(snake.length);
-        clearInterval(running);
-        frame=frame+0.5;
-        speed=1000/frame;
-        running= setInterval(start,speed);
-        food.rest();
-        if(food.x==snake.x&&food.y==snake.y){
-            food.rest();
+function gameStart(){
+    var worldSizeX =800;
+    var worldSizeY=600;
+    var snakeSize=20;
+    var foodSize=20;
+    var beginFramr=2;
+    var frame = beginFramr;
+    var speed = 1000/frame;
+    var maxX=worldSizeX/snakeSize-1;
+    var maxY=worldSizeY/snakeSize-1;
+    var theWorld=$("#World");
+    var theScore=$("#theScore");
+    var tempKey;
+    theWorld.css({'width':worldSizeX,'height': worldSizeY});
+    theScore.css({'left':(worldSizeX/2-25),'top':(worldSizeY+50)}).text("0");
+    var snake=new Snake(theWorld,'red','rgb(150,0,0)',snakeSize,snakeSize,maxX,maxY);
+    snake.rest();
+    var food=new Food(theWorld,'green',foodSize,foodSize,maxX,maxY);
+    food.rest();
+    if (food.x==snake.x&&food.y==snake.y)food.rest();
+    function start(){
+        if (tempKey==37) {
+            snake.turnLeft();//left
         }
-        for (var i = 0; i < snake.length; i++) {
-            if(food.x==snake.lastX[i]&&food.y==snake.lastY[i]){
+        if (tempKey==38) {
+            snake.turnUp();//top
+        }
+        if (tempKey==39) {
+            snake.turnRight();;//right
+        }
+        if (tempKey==40) {
+            snake.turnDown();//down
+        }
+        snake.update();
+        if(snake.eat(food.x,food.y)){
+            theScore.text(snake.length);
+            clearInterval(running);
+            frame=frame+0.5;
+            speed=1000/frame;
+            running= setInterval(start,speed);
+            food.rest();
+            if(food.x==snake.x&&food.y==snake.y){
                 food.rest();
             }
+            for (var i = 0; i < snake.length; i++) {
+                if(food.x==snake.lastX[i]&&food.y==snake.lastY[i]){
+                    food.rest();
+                }
+            }
+        }
+        if(!snake.isAlive){
+            alert("Game Over...");
+            theScore.text("0");
+            snake.removeBody();
+            snake.rest();
+            food.rest();
+            clearInterval(running);
+            frame=beginFramr;
+            speed=1000/frame;
+            running= setInterval(start,speed);
+            tempKey=null;
         }
     }
-    if(!snake.isAlive){
-        alert("Game Over...");
-        theScore.text("0");
-        snake.removeBody();
-        snake.rest();
-        food.rest();
-        clearInterval(running);
-        frame=beginFramr;
-        speed=1000/frame;
-        running= setInterval(start,speed);
-        tempKey=null;
-    }
+    var running= setInterval(start,speed);
+    $(document).keydown(function(event){
+        event.preventDefault();
+        tempKey=event.keyCode;
+    });
 }
-var running= setInterval(start,speed);
-$(document).keydown(function(event){
-	event.preventDefault();
-    tempKey=event.keyCode;
-});
