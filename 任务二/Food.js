60cd74999bf052f9dd1acdbb821414edc08e2c01
blunlove <314884680@qq.com>
2017-06-25 19:50:48 +0800

    在game里面对food和sanke的颜色属性进行传参，snake每吃一个食物都会加速

--- 任务二/Food.js
+++ 任务二/Food.js
@@ -1,12 +1,11 @@
-function Food(parent,w,h,mX,mY){
-	_this=this;
-	_this.color='green';
-	_this.element=$('<div>');
-	_this.element.css({'position': 'absolute','background-color':_this.color,'width':w,'height':h});
-	$(parent).append(_this.element);
-	_this.rest=function(){
-		_this.x=parseInt((Math.random()*mX));
-		_this.y=parseInt((Math.random()*mY));
-		_this.element.css({"left":_this.x*w,"top":_this.y*h});
+function Food(parent,color,w,h,mX,mY){
+	//this=this;
+	this.element=$('<div>');
+	this.element.css({'position': 'absolute','background-color':color,'width':w,'height':h});
+	$(parent).append(this.element);
+	this.rest=function(){
+		this.x=parseInt((Math.random()*mX));
+		this.y=parseInt((Math.random()*mY));
+		this.element.css({"left":this.x*w,"top":this.y*h});
 	}
 }
--- 任务二/Game.js
+++ 任务二/Game.js
@@ -1,4 +1,5 @@
-var speed = 100;
+var frame = 2;
+var speed = 1000/frame;
 var worldSizeX =800;
 var worldSizeY=600;
 var snakeSize=20;
@@ -10,9 +11,9 @@ var theScore=$("#theScore");
 var tempKey;
 theWorld.css({'width':worldSizeX,'height': worldSizeY});
 theScore.css({'left':(worldSizeX/2-25),'top':(worldSizeY+50)}).text("0");
-var snake=new Snake(theWorld,snakeSize,snakeSize,maxX,maxY);
-var food=new Food(theWorld,foodSize,foodSize,maxX,maxY);
+var snake=new Snake(theWorld,'red','rgb(150,0,0)',snakeSize,snakeSize,maxX,maxY);
 snake.rest();
+var food=new Food(theWorld,'green',foodSize,foodSize,maxX,maxY);
 food.rest();
 if (food.x==snake.x&&food.y==snake.y)food.rest();
 function start(){
@@ -31,6 +32,10 @@ function start(){
     snake.update();
     if(snake.eat(food.x,food.y)){
         theScore.text(snake.length);
+        clearInterval(running);
+        frame=frame+0.5;
+        speed=1000/frame;
+        running= setInterval(start,speed);
         food.rest();
         if(food.x==snake.x&&food.y==snake.y){
             food.rest();
@@ -50,8 +55,8 @@ function start(){
         tempKey=null;
     }
 }
-setInterval(start,speed);
+var running= setInterval(start,speed);
 $(document).keydown(function(event){
 	event.preventDefault();
-	tempKey=event.keyCode;
+    tempKey=event.keyCode;
 });
