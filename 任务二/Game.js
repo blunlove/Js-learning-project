f9aad08fcab3ef1d0bbfe0b03633b76dc3af821f
blunlove <314884680@qq.com>
2017-06-26 14:42:45 +0800

    添加双人竞赛功能

--- 任务二/Game.js
+++ 任务二/Game.js
@@ -1,69 +1,83 @@
 function gameStart(){
+    //var running1=null;
+    //var running2=null;
     var worldSizeX =800;
     var worldSizeY=600;
     var snakeSize=20;
     var foodSize=20;
-    var beginFramr=2;
-    var frame = beginFramr;
+    var frame = 100;
     var speed = 1000/frame;
     var maxX=worldSizeX/snakeSize-1;
     var maxY=worldSizeY/snakeSize-1;
     var theWorld=$("#World");
     var theScore=$("#theScore");
-    var tempKey;
     theWorld.css({'width':worldSizeX,'height': worldSizeY});
     theScore.css({'left':(worldSizeX/2-50),'top':(worldSizeY+30)}).text("0");
     var snake=new Snake(theWorld,'red','rgb(150,0,0)',snakeSize,snakeSize,maxX,maxY);
-    snake.rest();
+    var snake2=new Snake(theWorld,'rgb(150,0,0)','red',snakeSize,snakeSize,maxX,maxY);
+    function snakeRest(){
+        snake.rest();
+        snake2.rest();
+    }
+    snakeRest();
     var food=new Food(theWorld,'green',foodSize,foodSize,maxX,maxY);
     food.rest();
     if (food.x==snake.x&&food.y==snake.y)food.rest();
     function start(){
-        if (tempKey==37) {
-            snake.turnLeft();//left
-        }
-        if (tempKey==38) {
-            snake.turnUp();//top
-        }
-        if (tempKey==39) {
-            snake.turnRight();;//right
-        }
-        if (tempKey==40) {
-            snake.turnDown();//down
-        }
-        snake.update();
-        if(snake.eat(food.x,food.y)){
-            theScore.text(snake.length*100);
-            clearInterval(running);
-            frame=frame+0.5;
-            speed=1000/frame;
-            running= setInterval(start,speed);
-            food.rest();
-            if(food.x==snake.x&&food.y==snake.y){
-                food.rest();
-            }
+        function snakeStart(snake){
+            snake.element.css({"left":snake.x*snakeSize,"top":snake.y*snakeSize});
             for (var i = 0; i < snake.length; i++) {
-                if(food.x==snake.lastX[i]&&food.y==snake.lastY[i]){
+                snake.bodyElement[i].css({'left':snake.lastX[i]*snakeSize,'top':snake.lastY[i]*snakeSize});
+            }
+            if(snake.eat(food.x,food.y)){
+                theScore.text(snake.length*100);
+                food.rest();
+                if(food.x==snake.x&&food.y==snake.y){
                     food.rest();
                 }
+                for (var i = 0; i < snake.length; i++) {
+                    if(food.x==snake.lastX[i]&&food.y==snake.lastY[i]){
+                        food.rest();
+                    }
+                }
+            }
+            snake.bumpBody();
+            if(!snake.isAlive){
+                alert("Game Over...");
+                theScore.text("0");
+                snakeRest();
+                food.rest();
             }
         }
-        if(!snake.isAlive){
-            alert("Game Over...");
-            theScore.text("0");
-            snake.removeBody();
-            snake.rest();
-            food.rest();
-            clearInterval(running);
-            frame=beginFramr;
-            speed=1000/frame;
-            running= setInterval(start,speed);
-            tempKey=null;
-        }
+        snakeStart(snake);
+        snakeStart(snake2);
     }
     var running= setInterval(start,speed);
     $(document).keydown(function(event){
         event.preventDefault();
-        tempKey=event.keyCode;
+        if(event.keyCode==37){
+            snake.tempKey=37;
+        }
+        if(event.keyCode==38){
+            snake.tempKey=38;
+        }
+        if(event.keyCode==39){
+            snake.tempKey=39;
+        }
+        if(event.keyCode==40){
+            snake.tempKey=40;
+        }
+        if(event.keyCode==65){
+            snake2.tempKey=37;
+        }
+        if(event.keyCode==87){
+            snake2.tempKey=38;
+        }
+        if(event.keyCode==68){
+            snake2.tempKey=39;
+        }
+        if(event.keyCode==83){
+            snake2.tempKey=40;
+        }
     });
 }
--- 任务二/Snake.js
+++ 任务二/Snake.js
@@ -1,85 +1,106 @@
 function Snake(parent,color,bodycolor,w,h,mX,mY){
-    this.element = $("<div>");
-    this.element.css({'position': 'absolute','background-color':color,'width':w,'height':h});
-    $(parent).append(this.element);
-    this.rest=function(){
-        this.length=0;
-        this.x=parseInt((Math.random()*mX));
-        this.y=parseInt((Math.random()*mY));
-        this.speedY=0;
-        this.speedX=0;
-        this.isAlive=true;
-        this.lastX=new Array();
-        this.lastY=new Array();
-        this.bodyElement=new Array();
-        this.element.css({"left":this.x*w,"top":this.y*h});
+    var _this=this;
+    _this.beginframe=2;
+    _this.tempKey=null;
+    _this.running=null;
+    _this.element = $("<div>");
+    _this.element.css({'position': 'absolute','background-color':color,'width':w,'height':h});
+    $(parent).append(_this.element);
+    _this.turnLeft=function(){
+        if(_this.directionX==1){return;}
+        _this.directionX=-1;
+        _this.directionY=0;
     }
-    this.turnLeft=function(){
-        if(this.speedX==1){return;}
-        this.speedX=-1;
-        this.speedY=0;
+    _this.turnRight=function(){
+        if(_this.directionX==-1){return;}
+        _this.directionX=1;
+        _this.directionY=0;
     }
-    this.turnRight=function(){
-        if(this.speedX==-1){return;}
-        this.speedX=1;
-        this.speedY=0;
+    _this.turnUp=function(){
+        if(_this.directionY==1){return;}
+        _this.directionX=0;
+        _this.directionY=-1;
     }
-    this.turnUp=function(){
-        if(this.speedY==1){return;}
-        this.speedX=0;
-        this.speedY=-1;
+    _this.turnDown=function(){
+        if(_this.directionY==-1){return;}
+        _this.directionX=0;
+        _this.directionY=1;
     }
-    this.turnDown=function(){
-        if(this.speedY==-1){return;}
-        this.speedX=0;
-        this.speedY=1;
+    _this.Grow=function() {
+        _this.bodyElement[_this.length-1] = $("<div>");
+        _this.bodyElement[_this.length-1].css({'position': 'absolute','background-color':bodycolor,'width':h,'height':h});
+        _this.bodyElement[_this.length-1].css({'left':_this.lastX[_this,length]*w,'top':_this.lastY[_this,length]*h});
+        $(parent).append(_this.bodyElement[_this.length-1]);
     }
-    this.update=function(){
-        if(this.length>0) {
-            for (var i = 0; i < this.length; i++) {
-                this.lastX[this.length-i]=this.lastX[this.length-i-1];
-                this.lastY[this.length-i]=this.lastY[this.length-i-1];
-            }
-        }
-        this.lastX[0]=this.x;
-        this.lastY[0]=this.y;
-        this.x=this.x+this.speedX;
-        this.y=this.y+this.speedY;
-        this.bumpBody();
-        this.element.css({"left":this.x*w,"top":this.y*h});
-        for (var i = 0; i < this.length; i++) {
-            this.bodyElement[i].css({'left':this.lastX[i]*w,'top':this.lastY[i]*h});
-        }
-    }
-    this.eat=function(x,y){
-        if(this.x==x && this.y==y){
-            this.length++;
-            this.Grow();
+    _this.eat=function(x,y){
+        if(_this.x==x && _this.y==y){
+            _this.length++;
+            _this.frame=_this.frame+0.5;
+            _this.speed=1000/_this.frame;
+            clearInterval(_this.running);
+            _this.running=setInterval(_this.update,_this.speed);
+            _this.Grow();
             return true;
         }
         else return false;
     }
-    this.Grow=function() {
-        this.bodyElement[this.length-1] = $("<div>");
-        this.bodyElement[this.length-1].css({'position': 'absolute','background-color':bodycolor,'width':h,'height':h});
-        this.bodyElement[this.length-1].css({'left':this.lastX[this,length]*w,'top':this.lastY[this,length]*h});
-        $(parent).append(this.bodyElement[this.length-1]);
-    }
-    this.removeBody=function(){
-        for (var i = 0; i <this.length; i++) {
-            this.bodyElement[i].remove();
+    _this.removeBody=function(){
+        for (var i = 0; i <_this.length; i++) {
+            _this.bodyElement[i].remove();
         }
     }
-    this.bumpBody=function(){
-        if(this.x<0||this.x>mX||this.y<0||this.y>mY){
-            this.isAlive=false;
+    _this.bumpBody=function(){
+        if(_this.x<0||_this.x>mX||_this.y<0||_this.y>mY){
+            _this.isAlive=false;
             return;
         }
-        for (var i = 3; i < this.length; i++) {
-            if(this.x==this.lastX[i] && this.y==this.lastY[i]){
-                this.isAlive=false;
+        for (var i = 3; i < _this.length; i++) {
+            if(_this.x==_this.lastX[i] && _this.y==_this.lastY[i]){
+                _this.isAlive=false;
                 return;
             }
         }
+    }
+    _this.update=function(){
+        if (_this.tempKey==37) {
+            _this.turnLeft();//left
+        }
+        if (_this.tempKey==38) {
+            _this.turnUp();//top
+        }
+        if (_this.tempKey==39) {
+            _this.turnRight();;//right
+        }
+        if (_this.tempKey==40) {
+            _this.turnDown();//down
+        }
+        if(_this.length>0) {
+            for (var i = 0; i < _this.length; i++) {
+                _this.lastX[_this.length-i]=_this.lastX[_this.length-i-1];
+                _this.lastY[_this.length-i]=_this.lastY[_this.length-i-1];
+            }
+        }
+        _this.lastX[0]=_this.x;
+        _this.lastY[0]=_this.y;
+        _this.x=_this.x+_this.directionX;
+        _this.y=_this.y+_this.directionY;
+    }
+    _this.rest=function(){
+        _this.length=0;
+        _this.tempKey=null;
+        _this.x=parseInt((Math.random()*mX));
+        _this.y=parseInt((Math.random()*mY));
+        _this.directionY=0;
+        _this.directionX=0;
+        _this.frame=_this.beginframe;
+        _this.speed=1000/_this.frame;
+        _this.isAlive=true;
+        _this.lastX=new Array();
+        _this.lastY=new Array();
+        _this.removeBody();
+        _this.bodyElement=new Array();
+        _this.element.css({"left":_this.x*w,"top":_this.y*h});
+        clearInterval(_this.running);
+        _this.running=setInterval(_this.update,_this.speed);
     }
 }
