b4da7c535e100896dd28284d49020dd6c1de6cbb
leochuan <ruochuan.leo@gmail.com>
2017-06-26 18:19:37 +0800

    随便改改

--- 任务二/Game.js
+++ 任务二/Game.js
@@ -1,69 +1,115 @@
 function gameStart(){
-    var worldSizeX =800;
-    var worldSizeY=600;
-    var snakeSize=20;
-    var foodSize=20;
-    var beginFramr=2;
-    var frame = beginFramr;
-    var speed = 1000/frame;
-    var maxX=worldSizeX/snakeSize-1;
-    var maxY=worldSizeY/snakeSize-1;
-    var theWorld=$("#World");
-    var theScore=$("#theScore");
+    var worldSizeX = 800;
+    var worldSizeY = 600;
+    var snakeSize = 20;
+    var foodSize = 20;
+    var frame = 60;
+    var speed = 1000 / frame;
+    var maxX = worldSizeX / snakeSize - 1;
+    var maxY = worldSizeY / snakeSize - 1;
+    var theWorld = $("#World");
+    var theScore = $("#theScore");
     var tempKey;
-    theWorld.css({'width':worldSizeX,'height': worldSizeY});
-    theScore.css({'left':(worldSizeX/2-50),'top':(worldSizeY+30)}).text("0");
-    var snake=new Snake(theWorld,'red','rgb(150,0,0)',snakeSize,snakeSize,maxX,maxY);
+    var tempKey2;
+
+    theWorld.css({'width' : worldSizeX,'height' : worldSizeY});
+    theScore.css({'left' : (worldSizeX/2-50), 'top' : (worldSizeY+30)}).text("0");
+    var snake=new Snake(theWorld,'red','rgb(150,0,0)', snakeSize, snakeSize, maxX, maxY);
+    var snake2=new Snake(theWorld, 'blue', 'blue', snakeSize, snakeSize, maxX, maxY);
+
+    var snakeBaseSpeed = 500; // 初始速度 500ms 移动一次
+    var foodAcc = 20; // 吃一个食物缩短20ms移动间隔
+    var s1Speed = snakeBaseSpeed;
+    var s2Speed = snakeBaseSpeed;
+
+    //用于记录时间间隔
+    var lastS1Update = 0;
+    var lastS2Update = 0;
+
+    var food=new Food(theWorld, 'green', foodSize, foodSize, maxX, maxY);
     snake.rest();
-    var food=new Food(theWorld,'green',foodSize,foodSize,maxX,maxY);
+    snake2.rest();
     food.rest();
-    if (food.x==snake.x&&food.y==snake.y)food.rest();
+    while (food.x != snake.x && food.y != snake.y 
+        && food.x != snake2.x && food.y != snake2.y) food.rest();
+
     function start(){
-        if (tempKey==37) {
-            snake.turnLeft();//left
+        switch (tempKey) {
+            case 37:
+                snake.turnLeft();
+                break;
+            case 38:
+                snake.turnUp();
+                break;
+            case 39:
+                snake.turnRight();
+                break;
+            case 40:
+                snake.turnDown();
+                break;
         }
-        if (tempKey==38) {
-            snake.turnUp();//top
+
+        switch (tempKey2) {
+            case 65:
+                snake2.turnLeft();
+                break;
+            case 87:
+                snake2.turnUp();
+                break;
+            case 68:
+                snake2.turnRight();
+                break;
+            case 83:
+                snake2.turnDown();
+                break;
         }
-        if (tempKey==39) {
-            snake.turnRight();;//right
-        }
-        if (tempKey==40) {
-            snake.turnDown();//down
-        }
-        snake.update();
-        if(snake.eat(food.x,food.y)){
-            theScore.text(snake.length*100);
-            clearInterval(running);
-            frame=frame+0.5;
-            speed=1000/frame;
-            running= setInterval(start,speed);
-            food.rest();
-            if(food.x==snake.x&&food.y==snake.y){
+
+        var time = new Date().getTime();
+        if (time - lastS1Update >= s1Speed) {
+            lastS1Update = time;
+            snake.update();
+            if(snake.eat(food.x, food.y)) {
+                theScore.text(snake.length * 100);
                 food.rest();
+                while (food.x != snake.x && food.y != snake.y) food.rest();
+                s1Speed = s1Speed - foodAcc;
             }
-            for (var i = 0; i < snake.length; i++) {
-                if(food.x==snake.lastX[i]&&food.y==snake.lastY[i]){
-                    food.rest();
-                }
+            if(!snake.isAlive) {
+                gameOver();
             }
         }
-        if(!snake.isAlive){
-            alert("Game Over...");
-            theScore.text("0");
-            snake.removeBody();
-            snake.rest();
-            food.rest();
-            clearInterval(running);
-            frame=beginFramr;
-            speed=1000/frame;
-            running= setInterval(start,speed);
-            tempKey=null;
+
+        if (time - lastS2Update >= s2Speed) {
+            lastS2Update = time;
+            snake2.update();
+            if(snake2.eat(food.x,food.y)) {
+                theScore.text(snake2.length * 100);
+                food.rest();
+                while (food.x != snake2.x && food2.y != snake2.y) food.rest();
+                s2Speed = s2Speed - foodAcc;
+            }
+            if(!snake.isAlive) {
+                gameOver();
+            }
         }
     }
-    var running= setInterval(start,speed);
-    $(document).keydown(function(event){
+
+    function gameOver() {
+        alert("Game Over...");
+        tempKey = null;
+        tempKey2 = null; 
+        theScore.text("0");
+        snake.removeBody();
+        snake.rest();
+        snake2.removeBody();
+        snake2.rest();
+        food.rest();
+    }
+
+    setInterval(start, speed);
+    $(document).keydown(function(event) {
         event.preventDefault();
-        tempKey=event.keyCode;
+        tempKey = event.keyCode;
+        tempKey2 = event.keyCode;
     });
 }
