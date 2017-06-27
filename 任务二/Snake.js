3d798f6aa43a627a9599e293dc7399f7a4bcd263
leochuan <ruochuan.leo@gmail.com>
2017-06-27 10:18:46 +0800

    删除多余文件

--- 任务二/Snake.js
+++ 任务二/Snake.js
@@ -1,67 +1,88 @@
 function Snake(parent,color,bodycolor,w,h,mX,mY){
-    var _this=this;
-    _this.beginframe=2;
-    _this.tempKey=null;
-    _this.running=null;
-    _this.element = $("<div>");
-    _this.element.css({'position': 'absolute','background-color':color,'width':w,'height':h});
-    $(parent).append(_this.element);
-    _this.turnLeft=function(){
-        if(_this.directionX==1){return;}
-        _this.directionX=-1;
-        _this.directionY=0;
+    this.element = $("<div>");
+    this.element.css({'position': 'absolute','background-color':color,'width':w,'height':h});
+    $(parent).append(this.element);
+    this.rest=function(){
+        this.length=0;
+        this.x=parseInt((Math.random()*mX));
+        this.y=parseInt((Math.random()*mY));
+        this.speedY=0;
+        this.speedX=0;
+        this.isAlive=true;
+        this.lastX=new Array();
+        this.lastY=new Array();
+        this.bodyElement=new Array();
+        this.element.css({"left":this.x*w,"top":this.y*h});
     }
-    _this.turnRight=function(){
-        if(_this.directionX==-1){return;}
-        _this.directionX=1;
-        _this.directionY=0;
+    this.turnLeft=function(){
+        if(this.speedX==1){return;}
+        this.speedX=-1;
+        this.speedY=0;
     }
-    _this.turnUp=function(){
-        if(_this.directionY==1){return;}
-        _this.directionX=0;
-        _this.directionY=-1;
+    this.turnRight=function(){
+        if(this.speedX==-1){return;}
+        this.speedX=1;
+        this.speedY=0;
     }
-    _this.turnDown=function(){
-        if(_this.directionY==-1){return;}
-        _this.directionX=0;
-        _this.directionY=1;
+    this.turnUp=function(){
+        if(this.speedY==1){return;}
+        this.speedX=0;
+        this.speedY=-1;
     }
-    _this.Grow=function() {
-        _this.bodyElement[_this.length-1] = $("<div>");
-        _this.bodyElement[_this.length-1].css({'position': 'absolute','background-color':bodycolor,'width':h,'height':h});
-        _this.bodyElement[_this.length-1].css({'left':_this.lastX[_this,length]*w,'top':_this.lastY[_this,length]*h});
-        $(parent).append(_this.bodyElement[_this.length-1]);
+    this.turnDown=function(){
+        if(this.speedY==-1){return;}
+        this.speedX=0;
+        this.speedY=1;
     }
-    _this.eat=function(x,y){
-        if(_this.x==x && _this.y==y){
-            _this.length++;
-            _this.frame=_this.frame+0.5;
-            _this.speed=1000/_this.frame;
-            clearInterval(_this.running);
-            _this.running=setInterval(_this.update,_this.speed);
-            _this.Grow();
+    this.update=function(){
+        if(this.length>0) {
+            for (var i = 0; i < this.length; i++) {
+                this.lastX[this.length-i]=this.lastX[this.length-i-1];
+                this.lastY[this.length-i]=this.lastY[this.length-i-1];
+            }
+        }
+        this.lastX[0]=this.x;
+        this.lastY[0]=this.y;
+        this.x=this.x+this.speedX;
+        this.y=this.y+this.speedY;
+        this.bumpBody();
+        this.element.css({"left":this.x*w,"top":this.y*h});
+        for (var i = 0; i < this.length; i++) {
+            this.bodyElement[i].css({'left':this.lastX[i]*w,'top':this.lastY[i]*h});
+        }
+    }
+    this.eat=function(x,y){
+        if(this.x==x && this.y==y){
+            this.length++;
+            this.Grow();
             return true;
         }
         else return false;
     }
-    _this.removeBody=function(){
-        for (var i = 0; i <_this.length; i++) {
-            _this.bodyElement[i].remove();
+    this.Grow=function() {
+        this.bodyElement[this.length-1] = $("<div>");
+        this.bodyElement[this.length-1].css({'position': 'absolute','background-color':bodycolor,'width':h,'height':h});
+        this.bodyElement[this.length-1].css({'left':this.lastX[this,length]*w,'top':this.lastY[this,length]*h});
+        $(parent).append(this.bodyElement[this.length-1]);
+    }
+    this.removeBody=function(){
+        for (var i = 0; i <this.length; i++) {
+            this.bodyElement[i].remove();
         }
     }
-    _this.bumpBody=function(){
-        if(_this.x<0||_this.x>mX||_this.y<0||_this.y>mY){
-            _this.isAlive=false;
+    this.bumpBody=function(){
+        if(this.x<0||this.x>mX||this.y<0||this.y>mY){
+            this.isAlive=false;
             return;
         }
-        for (var i = 3; i < _this.length; i++) {
-            if(_this.x==_this.lastX[i] && _this.y==_this.lastY[i]){
-                _this.isAlive=false;
+        for (var i = 3; i < this.length; i++) {
+            if(this.x==this.lastX[i] && this.y==this.lastY[i]){
+                this.isAlive=false;
                 return;
             }
         }
     }
-    _this.updateSize=function(_w,_h){
+    this.updateSize=function(_w,_h){
         w=_w;
         h=_h;
         this.element.css({'width':w,'height':h,"left":this.x*w,"top":this.y*h});
