12e1b9ed0cebbf3da587cbecc83be6cae56a88ae
blunlove <314884680@qq.com>
2017-07-15 09:42:15 +0800

    修复回头bug，加一个积分表

--- 任务二/Game.js
+++ 任务二/Game.js
@@ -9,11 +9,17 @@ function gameStart(){
     var maxY = worldSizeY / snakeSize - 1;
     var theWorld = $("#World");
     var theScore = $("#theScore");
+    var theScore2 = $("#theScore2");
+    var p1=$("#p1");
+    var p2=$("#p2");
     var tempKey;
     var tempKey2;
 
     theWorld.css({'width' : worldSizeX,'height' : worldSizeY,'margin': '0 auto'});
-    theScore.css({'left' : (worldSizeX/2-50), 'top' : (worldSizeY+30)}).text("0");
+    theScore.css({'left' : (worldSizeX/3-50), 'top' : (worldSizeY+30)}).text("0");
+    theScore2.css({'left' : (worldSizeX/3*2-50), 'top' : (worldSizeY+30)}).text("0");
+    p1.css({'left':(worldSizeX/3-15),'top' : (worldSizeY+80)});
+    p2.css({'left':(worldSizeX/3*2-15),'top' : (worldSizeY+80)});
     var snake=new Snake(theWorld,'red','rgb(150,0,0)', snakeSize, snakeSize, maxX, maxY);
     var snake2=new Snake(theWorld, 'blue', 'blue', snakeSize, snakeSize, maxX, maxY);
 
@@ -39,39 +45,23 @@ function gameStart(){
     }
     foodRest();
     function start(){
-        switch (tempKey) {
-            case 37:
-            snake.turnLeft();
-            break;
-            case 38:
-            snake.turnUp();
-            break;
-            case 39:
-            snake.turnRight();
-            break;
-            case 40:
-            snake.turnDown();
-            break;
-        }
-
-        switch (tempKey2) {
-            case 65:
-            snake2.turnLeft();
-            break;
-            case 87:
-            snake2.turnUp();
-            break;
-            case 68:
-            snake2.turnRight();
-            break;
-            case 83:
-            snake2.turnDown();
-            break;
-        }
-
         var time = new Date().getTime();
         if (time - lastS1Update >= s1Speed) {
             lastS1Update = time;
+            switch (tempKey) {
+                case 37:
+                snake.turnLeft();
+                break;
+                case 38:
+                snake.turnUp();
+                break;
+                case 39:
+                snake.turnRight();
+                break;
+                case 40:
+                snake.turnDown();
+                break;
+            }
             snake.update();
             if(snake.eat(food.x, food.y)) {
                 theScore.text(snake.length * 100);
@@ -83,12 +73,25 @@ function gameStart(){
                 gameOver();
             }
         }
-
         if (time - lastS2Update >= s2Speed) {
             lastS2Update = time;
+            switch (tempKey2) {
+                case 65:
+                snake2.turnLeft();
+                break;
+                case 87:
+                snake2.turnUp();
+                break;
+                case 68:
+                snake2.turnRight();
+                break;
+                case 83:
+                snake2.turnDown();
+                break;
+            }
             snake2.update();
             if(snake2.eat(food.x,food.y)) {
-                theScore.text(snake2.length * 100);
+                theScore2.text(snake2.length * 100);
                 food.rest();
                 foodRest();
                 s2Frame = s2Frame+0.5;
@@ -106,6 +109,7 @@ function gameStart(){
         tempKey = null;
         tempKey2 = null; 
         theScore.text("0");
+        theScore2.text("0");
         snake.removeBody();
         snake.rest();
         snake2.removeBody();
@@ -121,8 +125,12 @@ function gameStart(){
     setInterval(start, speed);
     $(document).keydown(function(event) {
         event.preventDefault();
-        tempKey = event.keyCode;
-        tempKey2 = event.keyCode;
+        if(event.keyCode==37||event.keyCode==38||event.keyCode==39||event.keyCode==40){
+            tempKey = event.keyCode;
+        }
+        if(event.keyCode==65||event.keyCode==87||event.keyCode==68||event.keyCode==83){
+            tempKey2 = event.keyCode;
+        }
     });
 
     //根据窗口大小调整世界的比例
@@ -132,7 +140,10 @@ function gameStart(){
         var snakeSize = worldSizeX/40;
         var foodSize = worldSizeX/40;
         theWorld.css({'width' : worldSizeX,'height' : worldSizeY});
-        theScore.css({'left' : (worldSizeX/2-50), 'top' : (worldSizeY+30)})
+        theScore.css({'left' : (worldSizeX/3-50), 'top' : (worldSizeY+30)})
+        theScore2.css({'left' : (worldSizeX/3*2-50), 'top' : (worldSizeY+30)})
+        p1.css({'left':(worldSizeX/3-15),'top' : (worldSizeY+80)});
+        p2.css({'left':(worldSizeX/3*2-15),'top' : (worldSizeY+80)});
         snake.updateSize(snakeSize,snakeSize);
         snake2.updateSize(snakeSize,snakeSize);
         food.updateSize(snakeSize,snakeSize);
