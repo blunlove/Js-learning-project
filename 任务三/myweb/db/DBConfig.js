d8babcc08039fb0a66f5dffeb10ef53224980e4a
blunlove <314884680@qq.com>
2017-07-24 14:09:08 +0800

    初步实现数据库对接

---
+++ 任务三/myweb/db/DBConfig.js
@@ -0,0 +1,11 @@
+module.exports =
+{  
+	mysql:
+	{   
+		host: '127.0.0.1',
+		user: 'root',
+		password: 'alun123456',
+		database:'Expressdb',
+		port: 3306
+	}
+};
---
+++ 任务三/myweb/db/usersql.js
@@ -0,0 +1,7 @@
+var UserSQL = 
+{  
+	insert:'INSERT INTO User(uid,userName) VALUES(?,?)', 
+	queryAll:'SELECT * FROM User',  
+	getUserById:'SELECT * FROM User WHERE uid = ? ',
+};
+module.exports = UserSQL;
