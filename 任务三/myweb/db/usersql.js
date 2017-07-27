74d4240f0291a093d64ffcba791af2556b302ee8
blunlove <314884680@qq.com>
2017-07-27 11:24:28 +0800

    添加了数据库的增删查改功能

--- 任务三/myweb/db/usersql.js
+++ 任务三/myweb/db/usersql.js
@@ -1,6 +1,8 @@
 var UserSQL = 
 {  
 	insert:'INSERT INTO User(uid,userName) VALUES(?,?)', 
+	delete:'DELETE FROM User WHERE uid= ? ',
+	update:'UPDATE User SET userName =? WHERE uid= ? ',
 	queryAll:'SELECT * FROM User',  
 	getUserById:'SELECT * FROM User WHERE uid = ? ',
 };
