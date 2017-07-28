48f65edd4fe9d7b8b7fa34b8c4ad426c91076526
blunlove <314884680@qq.com>
2017-07-28 15:41:00 +0800

    增加登录完成时页面

--- 任务三/myweb/db/usersql.js
+++ 任务三/myweb/db/usersql.js
@@ -2,8 +2,10 @@ var UserSQL =
 {  
 	insert:'INSERT INTO User(userName,passWord) VALUES(?,?)', 
 	delete:'DELETE FROM User WHERE userName= ? ',
-	insertGoods:'INSERT INTO goods(goodsName) VALUES(?)',
 	queryAll:'SELECT * FROM User',
 	getUserById:'SELECT * FROM User WHERE uid = ? ',
+
+	insertGoods:'INSERT INTO goods(goodsName) VALUES(?)',
+	queryAllGoods:'SELECT * FROM goods',
 };
 module.exports = UserSQL;
--- 任务三/myweb/routes/login.js
+++ 任务三/myweb/routes/login.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+var dbConfig = require('../db/DBConfig');
+var userSQL = require('../db/Usersql');
+
+var pool = mysql.createPool(dbConfig.mysql);
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('login');
+	pool.getConnection(function(err, connection){
+		connection.query(userSQL.queryAllGoods, function(err, result){
+			res.render('login',{msg:result});
+			connection.release();
+		});
+	});
 });
 
 module.exports = router;
