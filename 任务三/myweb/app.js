3bf5392c587659c6d360e7331fb6b0a4893397b7
blunlove <314884680@qq.com>
2017-07-28 14:26:15 +0800

    上传界面css修改

--- 任务三/myweb/app.js
+++ 任务三/myweb/app.js
@@ -18,7 +18,6 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(require('connect-multiparty')());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
