42e0eaa3bb0c6d2aa6d0ce94a8f81a861b84273c
blunlove <314884680@qq.com>
2017-08-11 17:26:35 +0800

    修改

--- python/hello.py
+++ python/hello.py
@@ -1,7 +1,9 @@
-def calc(*numbers):
-    summ = 0
-    for n in numbers:
-        summ = summ + n * n
-    return summ
-nums = [1, 2, 3]
-print(calc(*nums))
+def fib(max):
+    n,a,b=0,0,1
+    while n<max:
+        yield b
+        a,b=b,a+b
+        n=n+1
+    return 'done'
+for n in fib(8):
+    print(n)
--- q4(branch)/text.js
+++ q4(branch)/text.js
@@ -1,3 +1,21 @@
+const zhishu = (thenumber) => {
+    let temp = 0;
+    let y = [];
+    let n = thenumber;
+    while (n > -1) {
+        y.push(n);
+        n--;
+    }
+    for (let the in y) {
+        if (thenumber % the == 0) {
+            temp++;
+        }
+    }
+    if (temp == 2) {
+        console.log(thenumber);
+    }
+}
 
-
-
+for (let i = 2; ; i++) {
+    zhishu(i);
+}
