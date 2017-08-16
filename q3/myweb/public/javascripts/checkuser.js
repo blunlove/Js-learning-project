1dfb1f95120b843054d72d5d51acb78468ae3b8e
blunlove <314884680@qq.com>
2017-08-16 14:33:56 +0800

    输完密码按回车键能直接登录

--- q3/myweb/public/javascripts/checkuser.js
+++ q3/myweb/public/javascripts/checkuser.js
@@ -3,9 +3,11 @@ const checkSend = () => {
 	let passWord = $("#psw").val();
 	if (userName.length == 0) {
 		alert('账号不能为空');
+		$('.text').val("");
 		return;
 	} else if (passWord.length == 0) {
 		alert('密码不能为空');
+		$('.text').val("");
 		return;
 	}
 	$.post('/users/checkUser', { 'userName': userName, 'passWord': passWord, 'islogin': true }, (res) => {
