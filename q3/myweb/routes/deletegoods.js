a036b3b0b64e726dfb760ca9b88f1694b4d1401c
blunlove <314884680@qq.com>
2017-08-17 09:59:12 +0800

    删除数据库数据的同时删除相应的文件

--- q3/myweb/routes/deletegoods.js
+++ q3/myweb/routes/deletegoods.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let router = express.Router();
+let fs = require('fs');
 
 let mysql = require('mysql');
 let dbConfig = require('../db/DBConfig');
@@ -12,18 +13,34 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/delete', (req, res, next) => {
-    pool.getConnection((err, connection) => {
-		console.log("ok");
+	pool.getConnection((err, connection) => {
 		let delete_goodsName = req.body.goodsName;
-        connection.query(userSQL.delete, [delete_goodsName], (err, result) => {
-            if (result.affectedRows) {
-                res.json({ delete: 'success', msg: '删除成功' });
-            } else {
-                res.json({ delete: 'fail', msg: '该商品不存在' });
-            }
-            connection.release();
-        });
-    });
+		connection.query(userSQL.queryGood, [delete_goodsName], (err, result) => {
+			let goodsPic, goodsDetail;
+			if (result) {
+				goodsPic = result[0].goodsPic;
+				goodsDetail = result[0].goodsDetail;
+				connection.query(userSQL.delete, [delete_goodsName], (err, result) => {
+					if (result.affectedRows) {
+						console.log(result);
+						res.json({ delete: 'success', msg: '删除成功' });
+						fs.unlink(goodsPic,()=>{
+							console.log("删除商品图片成功");
+						});
+						fs.unlink(goodsDetail,()=>{
+							console.log("删除商品详情成功");
+						});
+					} else {
+						res.json({ delete: 'fail', msg: '删除失败' });
+					}
+				});
+			} else {
+				res.json({ delete: 'fail', msg: '该商品不存在' });
+			}
+			connection.release();
+		});
+
+	});
 });
 
 module.exports = router;
