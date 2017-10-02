fa18b2c09e1658b7be21e0f3d0efa9b4cddb1e88
blunlove <314884680@qq.com>
2017-10-02 19:05:01 +0800

    修改

--- test/test.js
+++ test/test.js
@@ -146,7 +146,7 @@ const _main = () => {
     setInterval(() => {
         let keydowns = Object.keys(game.keydowns);
         let lastKey = keydowns[keydowns.length - 1];
-        if (lastKey && game.actions[lastKey] != undefined) {
+        if (lastKey && game.actions[lastKey]) {
             man.moving();
             game.actions[lastKey]();
         } else {
