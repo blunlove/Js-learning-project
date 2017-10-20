fba8c6fe6b0bfe7ac9d667b0ee40b0b01d2f1d24
blunlove <314884680@qq.com>
2017-10-20 16:25:18 +0800

    菜单弹出子菜单

--- q6(vue-webpack)/src/components/header.vue
+++ q6(vue-webpack)/src/components/header.vue
@@ -12,6 +12,9 @@
                             <a class="i-link">
                                 <span>{{ menu.name }}</span>
                             </a>
+                            <div v-if="menu.child" :class="menu.child_class">
+                                {{ menu.child_class.substr(11) }}
+                            </div>
                         </li>
                     </ul>
                 </div>
@@ -22,25 +25,24 @@
     </div>
 </template>
 <script>
+let menus = [
+    { name: '主站', class: 'menu home', child: false },
+    { name: '画友', class: 'menu friend', child: false },
+    { name: '游戏中心', class: 'menu game', child: true, child_class: 'child_menu game-page' },
+    { name: '直播', class: 'menu live', child: true, child_class: 'child_menu live-page' },
+    { name: '会员购', class: 'menu pay', child: false },
+    { name: '周边', class: 'menu real-things', child: false },
+    { name: '移动端', class: 'menu mobile', child: true, child_class: 'child_menu mobile-page' },
+]
 export default {
     name: 'headerMenu_left',
     data() {
         return {
-            menus: [
-                { name: '主站', class: 'home' },
-                { name: '画友', class: 'friend' },
-                { name: '游戏中心', class: 'game' },
-                { name: '直播', class: 'live' },
-                { name: '会员购', class: 'pay' },
-                { name: '周边', class: 'real-things' },
-                { name: '移动端', class: 'mobile' },
-            ]
+            menus: menus
         }
     },
     methods: {
-        eClick() {
-            console.log(9999);
-        }
+
     }
 }
 </script>
