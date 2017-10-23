d439334fae9f5958284614d3767022ada9cd9021
blunlove <314884680@qq.com>
2017-10-23 15:27:49 +0800

    头像缩放

--- q6(vue-webpack)/src/components/header.vue
+++ q6(vue-webpack)/src/components/header.vue
@@ -8,7 +8,7 @@
             <div class="header_line_menu">
                 <div class="header_line_menu_left">
                     <ul>
-                        <li v-for="menu in menus" :class="'menu ' + menu.class">
+                        <li v-for="menu in menus_left" :class="'menu_left ' + menu.class">
                             <a class="i-link">
                                 <span>{{ menu.name }}</span>
                             </a>
@@ -24,7 +24,18 @@
                         </li>
                     </ul>
                 </div>
-                <div class="header_line_menu_right"></div>
+                <div class="header_line_menu_right">
+                    <ul>
+                        <li v-for="menu in menus_right" :class="'menu_right ' + menu.class">
+                            <a class="i-link">
+                                <span>{{ menu.name }}</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="userPortrait">
+                    <div class="userPortrait_image"></div>
+                </div>
             </div>
         </div>
         <a id="logo"></a>
@@ -34,20 +45,27 @@
 import gameMenu from '../child_menu/game.vue';
 import liveMenu from '../child_menu/live.vue';
 import mobileMenu from '../child_menu/mobile.vue';
-let menus = [
-    { name: '主站', class: 'home' },
-    { name: '画友', class: 'friend' },
-    { name: '游戏中心', class: 'game' },
-    { name: '直播', class: 'live' },
-    { name: '会员购', class: 'pay' },
-    { name: '周边', class: 'real-things' },
-    { name: '移动端', class: 'mobile' },
-]
 export default {
     name: 'headerMenu_left',
     data() {
         return {
-            menus: menus
+            menus_left: [
+                { name: '主站', class: 'home' },
+                { name: '画友', class: 'friend' },
+                { name: '游戏中心', class: 'game' },
+                { name: '直播', class: 'live' },
+                { name: '会员购', class: 'pay' },
+                { name: '周边', class: 'real-things' },
+                { name: '移动端', class: 'mobile' },
+            ],
+            menus_right: [
+                { name: '消息', class: 'message' },
+                { name: '动态', class: 'dynamic' },
+                { name: '稍后再看', class: 'later' },
+                { name: '收藏夹', class: 'favorite' },
+                { name: '历史', class: 'history' },
+                { name: '投稿', class: 'submission' },
+            ]
         }
     },
     methods: {
