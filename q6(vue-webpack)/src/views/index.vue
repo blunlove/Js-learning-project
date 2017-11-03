adba5499ff244c46801c92ebba93b816017d5360
blunlove <314884680@qq.com>
2017-11-03 16:18:33 +0800

    完成导航栏(下载客户端未完成(mobile))

--- q6(vue-webpack)/src/views/index.vue
+++ q6(vue-webpack)/src/views/index.vue
@@ -1,16 +1,54 @@
 <template>
     <div class="body_index">
         <div>index</div>
-        <div class="mobilePhone"
-            @click="toTop()"
-            :style="style"
-            v-on:mouseover="mobilePhone_over()"
-            v-on:mouseleave="mobilePhone_leave()">
+        <div class="suoyin">
+            <div class="position_menu">
+                <ul>
+                    <li v-for="menu in position_menu" :class="menu.class">{{ menu.name }}</li>
+                    <li class="position_18">
+                        <div class="position_18_image">排序</div>
+                    </li>
+                </ul>
+            </div>
+            <div class="baffle"></div>
+            <div class="back_top">
+                <div class="back_top_image"></div>
+            </div>
+            <div class="mobilePhone"
+                @click="toTop()"
+                :style="style"
+                v-on:mouseover="mobilePhone_over()"
+                v-on:mouseleave="mobilePhone_leave()">
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+let position_menu = [
+    { name: '直播' },
+    { name: '动画' },
+    { name: '番剧' },
+    { name: '国创' },
+    { name: '音乐' },
+    { name: '舞蹈' },
+    { name: '游戏' },
+    { name: '科技' },
+    { name: '生活' },
+    { name: '鬼畜' },
+    { name: '时尚' },
+    { name: '广告' },
+    { name: '娱乐' },
+    { name: '电影' },
+    { name: 'TV剧' },
+    { name: '影视' },
+    { name: '纪录片' },
+];
+
+for (let i = 0; i < 17; i++) {
+    position_menu[i].class = 'position_' + (i + 1).toString();
+}
+
 let mobilePhone = {
     isAnimation: false,
     isLeaving: false,
@@ -45,6 +83,7 @@ export default {
     data() {
         return {
             style: '',
+            position_menu: position_menu,
         }
     },
     methods: {
