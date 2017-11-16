4271f50ebeddbde5659c9f65851ea9512cc777a2
blunlove <314884680@qq.com>
2017-11-16 15:38:35 +0800

    完成extension模块

--- q6(vue-webpack)/src/views/index.vue
+++ q6(vue-webpack)/src/views/index.vue
@@ -2,6 +2,7 @@
     <div class="body_index">
         <carousel></carousel
         ><hot-video></hot-video>
+        <extension></extension>
         <suoyin></suoyin>
     </div>
 </template>
@@ -10,10 +11,14 @@
 import Carousel from '../components/carousel.vue';
 import Suoyin from '../components/suoyin.vue';
 import HotVideo from '../components/hot_video.vue';
+import Extension from '../components/extension.vue';
 export default {
     name: 'index',
     components: {
-        Carousel, Suoyin, HotVideo
+        Carousel,
+        Suoyin,
+        HotVideo,
+        Extension,
     }
 }
 </script>
